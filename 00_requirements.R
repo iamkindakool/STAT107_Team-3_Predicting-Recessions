e22f3ca210b2798e204ba6347c2830dcf521d823
Shinian-03 <glh617026811@gmail.com>
2025-11-06 11:32:28 -0800

    Add files via upload

---
+++ 00_requirements.R
@@ -0,0 +1,19 @@
+
+
+library(fredr)
+install.packages("fredr", dependencies = TRUE)
+installed.packages()["fredr",]
+install.packages("fredr")
+##We install this package for knowing the US recession.
+
+library(dplyr)
+##Use it to code for data flames.
+
+library("ggplot2")
+install.packages("ggplot2")
+##use for visual data.
+
+install.packages("quantmod")
+library(quantmod)
+##This data is use for answer the Market stock and trade value.
+
