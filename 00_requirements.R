f0d9fd71d79c58b1acdd7072b29ff49174190a37
Shinian-03 <glh617026811@gmail.com>
2025-11-06 11:16:38 -0800

    Add files via upload

---
+++ 00_requirements.R
@@ -0,0 +1,9 @@
+ ## Install
+library(fredr)         ## Load package to environment.
+## Install
+library(dplyr)            ## Load package to environment.
+## Install
+library("ggplot2")            ## Load package to environment.
+## Install
+library(quantmod)            ## Load package to environment.
+
