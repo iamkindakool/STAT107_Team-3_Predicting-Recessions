1fdb08c1cd018c07bfa328c51adff3216ac77310
Shinian-03 <glh617026811@gmail.com>
2025-11-06 11:32:11 -0800

    Delete 00_requirements.R

--- 00_requirements.R
+++
@@ -1,9 +0,0 @@
- ## Install
-library(fredr)         ## Load package to environment.
-## Install
-library(dplyr)            ## Load package to environment.
-## Install
-library("ggplot2")            ## Load package to environment.
-## Install
-library(quantmod)            ## Load package to environment.
-
