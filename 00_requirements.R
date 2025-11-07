84615c5dff51115a5091603ffe838cba96edc2eb
iamkindakool <139824099+iamkindakool@users.noreply.github.com>
2025-11-06 17:46:09 -0800

    Add files via upload

--- 00_requirements.R
+++ 00_requirements.R
@@ -1,16 +1,15 @@
 
-
-library(fredr)
 install.packages("fredr", dependencies = TRUE)
 installed.packages()["fredr",]
 install.packages("fredr")
+library(fredr)
 ##We install this package for knowing the US recession.
 
 library(dplyr)
 ##Use it to code for data flames.
 
-library("ggplot2")
 install.packages("ggplot2")
+library("ggplot2")
 ##use for visual data.
 
 install.packages("quantmod")
